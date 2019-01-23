2907d495dce0d014456d1b17aebed802d3359e51
Gawan Dekker <gawan.dekker@gmail.com>
2019-01-23 11:56:08 +0100

    Added plotting of determined sample size information.

--- power_analysis.py
+++ power_analysis.py
@@ -7,6 +7,7 @@ import random
 from scipy import stats, sqrt
 from math import ceil
 
+import matplotlib.pyplot as plt
 
 def tmp_interleaving(pair):
     """Temporary interleaving function for simulation test.
@@ -119,8 +120,40 @@ def compute_sample_size(p1, alpha=0.05, beta=0.10):
     return ceil(n)
 
 
+def get_bin_labels(n_bins, n_decimals=3, cut_sides=0.0):
+    """Creates labels containing ranges for bins.
+    
+    Parameters
+    ----------
+    n_bins : int
+        Amount of bins to label.
+    n_decimals : int
+        Number of decimals to round the ranges on.
+    cut_sides : float
+        Amount by which first and last bin have their ranges cut.
+    
+    Returns
+    -------
+    out : list
+        A list containing labels.
+    """
+    out = []
+    step = 1.0 / n_bins
+    for i in range(n_bins):
+        # Determine delta range of bin.
+        dmin = i * step
+        dmax = dmin + step
+        if i == 0:
+            dmin += cut_sides
+        if i == n_bins - 1:
+            dmax -= cut_sides
+        dmin = round(dmin, n_decimals)
+        dmax = round(dmax, n_decimals)
+        out.append('[' + str(dmin) + ' - ' + str(dmax) + ')')
+    return out
+
 def process_bins(bins):
-    """Prints minimum value, median and maximum value
+    """Determines minimum value, maximum value and median
     for each of the given bins.
     
     Parameters
@@ -130,51 +163,125 @@ def process_bins(bins):
     
     Returns
     -------
-    None
+    out : list
+        A list of dictionaries containing the minimum value,
+        maximum value and median for each bin.
     """
-    step = 1.0 / len(bins)
-    for i, cur in enumerate(bins):
-        # Determine delta range of bin.
-        dmin = i * step
-        dmax = dmin + step
-        if i == 0:
-            dmin += step / 2
-        if i == len(bins) - 1:
-            dmax -= step / 2
-        dmin = round(dmin, 3)
-        dmax = round(dmax, 3)
-        range_str = '[' + str(dmin) + ' - ' + str(dmax) + ']'
-        print('BIN', range_str)
-        if len(cur) > 0:
+    out = []
+    for cur in bins:
+        if cur == []:
+            out.append({'has_info' : False})
+        else:
             # Sort bin and remove error-data.
             cur.sort()
             cur.reverse()
             while cur[-1] == -1:
                 cur.pop()
             cur.reverse()
-            # Determine and print minimum, median and maximum.
+            # Determine minimum, median and maximum.
             d, m = divmod(len(cur), 2)
             median = ceil((cur[d] + cur[-int(not bool(m))]) / 2.0)
-            print('     min', min(cur))
-            print('  median', median)
-            print('     max', max(cur))
+            out.append({'min' : min(cur), 'max' : max(cur),
+                'median' : median, 'has_info' : True})
+    return out
+
+def print_bin_info(bin_info, labels=None):
+    """Prints minimum value, median and maximum value
+    for each of the given bin info dictionaries.
+    
+    Parameters
+    ----------
+    bins : array_like
+        Array of dictionaries containing `min`, `max` and `median`
+        fields, also contains a `has_info` field which is to be turned
+        set `False` if not all other fields are present.
+        
+    Returns
+    -------
+    None
+    """
+    if labels == None:
+        labels = list(range(len(bin_info)))
+    for info, label in zip(bin_info, labels):
+        print('BIN', label)
+        if info['has_info']:
+            print('     min', info['min'])
+            print('  median', info['median'])
+            print('     max', info['max'])
         else:
             print('  NO DATA')
         print()
     return
 
 
+def plot_bin_info(bin_info_list, bin_set_labels=[], bin_labels=[]):
+    """Plots min/median/max information for the bins
+    in the given bin sets.
+    
+    Parameters
+    ----------
+    bin_info_list : array_like
+        A list of sets of bins containing dictionaries
+        with min/median/max/has_info information.
+    bin_set_labels : array_like
+        A list of labels for the different sets of bins.
+    bin_labels : array_like
+        A list of labels of the different bins in the bin sets.
+        Is used to label the x axis.
+    
+    Returns
+    -------
+    None
+    """
+    if bin_set_labels == []:
+        bin_set_labels = list(range(len(bin_info_list)))
+    # Plot median with min and max as error bars
+    # for each bin in each bin set.
+    for i, (bin_info, label) in enumerate(zip(bin_info_list, bin_set_labels)):
+        x = [j + 0.1 * (i + 1) for j in range(len(bin_info))]
+        y = [info['median'] if info['has_info'] else 0 for info in bin_info]
+        err = ([info['median'] - info['min'] if info['has_info'] else 0
+            for info in bin_info],
+            [info['max'] - info['median'] if info['has_info'] else 0
+            for info in bin_info])
+        plt.errorbar(x, y, err, label=label)
+    # Apply bin labels.
+    if bin_labels != []:
+        plt.xticks(range(len(bin_labels)), bin_labels + [''], rotation=30)
+    plt.legend()
+    plt.xlabel('$\Delta$ERR')
+    plt.ylabel('sample size')
+    plt.title('Determined sample size')
+    plt.tight_layout()
+    plt.show()
+    return
+
+
 def main():
+    n_bins = 10
+    cut_sides = 0.05
     inputs = generate_input.gen_input_pairs(3, 2)
-    bins = [[] for _ in range(10)]
+    bins = [[[] for _ in range(n_bins)] for _ in range(4)]
+    # Obtain data through simulations.
     for pair in inputs:
         dERR = generate_input.ERR(pair[1]) - generate_input.ERR(pair[0])
-        if dERR >= 0.05 and dERR <= 0.95:
+        if dERR >= cut_sides and dERR < 1.0 - cut_sides:
             # Assumes no duplicates for now.
-            p = interleaving_simulation(
-                pair, 1000, tmp_interleaving, tmp_click_model)
-            bins[int(dERR * 10)].append(compute_sample_size(p))
-    process_bins(bins)
+            for cur_bins in bins:
+                p = interleaving_simulation(
+                    pair, 50000, tmp_interleaving, tmp_click_model)
+                cur_bins[int(dERR * 10)].append(compute_sample_size(p))
+    # Process bins.
+    bin_labels = get_bin_labels(n_bins, cut_sides=cut_sides)
+    bin_info_list = []
+    bin_set_labels = [str(i) for i in range(len(bin_info_list))]
+    for i, cur_bins in enumerate(bins):
+        print('RUN ' + str(i) + '\n=====\n')
+        bin_info =  process_bins(cur_bins)
+        print_bin_info(bin_info, bin_labels)
+        bin_info_list.append(bin_info)
+    # Plot bin information.
+    plot_bin_info(bin_info_list, bin_set_labels, bin_labels)
     return
 
 
